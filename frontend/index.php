62503c704bb44a69909cf72111757bc2dd405b65
Taha Tekriti <tekrititaha@gmail.com>
2024-01-03 14:15:18 +0200

    name_edited

--- frontend/index.php
+++ frontend/index.php
@@ -64,7 +64,7 @@
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
                     Unlock Your Potential Ignite Your Mind</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From
-                    Empower your journey through knowledge with [Your E-Learning Platform Name] where education meets
+                    Empower your journey through knowledge with SkillStreem where education meets
                     innovation.</p>
 
                 <button onclick="window.location.href = '../frontend/courses.php';" class=" shadow__btn inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 border border-white">
